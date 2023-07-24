374d96192f42ab02956fe9021efb559906660744
XinzhouLi <48045064+XinzhouLi@users.noreply.github.com>
2023-07-24 02:25:35 -0600

    working on 4sum

--- answers/17.letter-combinations-of-a-phone-number.py
+++ answers/17.letter-combinations-of-a-phone-number.py
@@ -6,6 +6,15 @@
 
 # @lc code=start
 class Solution:
+    def wfs(self, digits, map, previous, current):
+        result = []
+        if current == len(digits):
+            return [previous]
+        else:
+            for char in map[int(digits[current])]:
+
+                result.extend(self.wfs(digits, map, previous+char, current+1))
+            return result
     def letterCombinations(self, digits: str) -> List[str]:
         init = ord('a')
         map = {}
@@ -16,16 +25,9 @@ class Solution:
                 continue
             map[i] = [chr(j) for j in range(init, init+3)]
             init += 3
-        return wfs(digits, map, "", 0)
+        if digits == "":
+            return []
+        return self.wfs(digits, map, "", 0)
 
-    def wfs( self, digits, map, previous, current):
-        result = []
-        if current == len(digits):
-            return [previous]
-        else:
-            for char in map[int(digits[current])]:
-
-                result.extend(wfs(digits, map, previous+char, current+1))
-            return result
 # @lc code=end
 
---
+++ answers/18.4-sum.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=18 lang=python3
+#
+# [18] 4Sum
+#
+
+# @lc code=start
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        indexMap = {}
+        for (index, value) in enumerate(nums):
+            print(index, value)
+# @lc code=end
+
