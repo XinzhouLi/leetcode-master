136be2ddf8c6b3f5b058113dccb00ceb786f1f49
Old Li's Mac <xinzhou.li9822@gmail.com>
2023-07-27 14:09:10 -0600

    update 4sum

--- answers/18.4-sum.py
+++ answers/18.4-sum.py
@@ -9,6 +9,32 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         indexMap = {}
         for (index, value) in enumerate(nums):
-            print(index, value)
+            if value in indexMap.keys():
+                indexMap[value].append(index)
+            else: 
+                indexMap[value] = [index]
+        nums.sort()
+        valueResult = []
+        p4 = len(nums)-1
+        for p1 in range(p4-1):
+            if nums[p1] > target:
+                continue
+            for p2 in range(p1+1,p4-1):
+                if nums[p1] + nums[p2] > target:
+                    continue
+                p3 = p2+1
+                while p3<p4:
+                    sum = nums[p1] + nums[p2] + nums[p3] + nums[p4]
+                    valueAns = [nums[p1], nums[p2], nums[p3], nums[p4]]
+                    if sum < target:
+                        p3 += 1
+                    elif sum > target:
+                        p4 -= 1
+                    elif sum == target and valueAns in valueResult:
+                        p3 += 1
+                        p4 -= 1
+                    else:
+                        valueResult.append(valueAns)
+        return valueResult
 # @lc code=end
 
